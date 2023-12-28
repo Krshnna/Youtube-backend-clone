c9030aaad23b9352a2574355cdc66d794ec0b2c8
Krishna Kapoor <88818689+Krshnna@users.noreply.github.com>
2023-12-28 10:23:54 +0530

    config env

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,54 +1,31 @@
-// import { v2 as cloudinary } from "cloudinary";
-// import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import dotenv from "dotenv";
 
 
+dotenv.config({
+  path: "./.env",
+});
 
-
-// const UploadOnCloudinary = async (localFilePath) => {
-//   try {
-//     if (!localFilePath) return null;
-//     const response = await cloudinary.uploader.upload(localFilePath, {
-//       resource_type: "auto",
-//     });
-//     console.log("File is uploaded", response);
-//     return response;
-//   } catch (error) {
-//     fs.unlinkSync(localFilePath);
-//     return null;
-//   }
-// };
-// export default UploadOnCloudinary;
-
-
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
-// cloudinary.config({ 
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-//   api_key: process.env.CLOUDINARY_API_KEY, 
-//   api_secret: process.env.CLOUDINARY_API_SECRET 
-// });
-          
-cloudinary.config({ 
-  cloud_name: 'dm4te935a', 
-  api_key: '994755216792626', 
-  api_secret: 'IbipxEgN8J-ZJrrgWr14oj0INwk' 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const UploadOnCloudinary = async (localFilePath) => {
-    try {
-        if (!localFilePath) return null
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
-        })
-        fs.unlinkSync(localFilePath);
-        return response;
-    } catch (error) {
-      console.log(error);
-        fs.unlinkSync(localFilePath) 
-        return null;
-    }
-}
-
-
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    console.log(error);
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
 
 export default UploadOnCloudinary;
