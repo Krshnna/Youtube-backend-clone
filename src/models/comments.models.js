dc55552f3f348e24435777c4015a5a7ed97b1e6f
Krishna Kapoor <88818689+Krshnna@users.noreply.github.com>
2024-02-07 11:23:09 +0530

    added comment model, controllers and routes

---
+++ src/models/comments.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    videoId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+commentSchema.plugin(mongooseAggregatePaginate);
+
+export const Comment = mongoose.model("Comment", commentSchema);
